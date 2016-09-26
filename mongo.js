92a119868b9e21afc76e6d0a287156fee78cfb7d
tajima12 <zakichan1216@gmail.com>
2016-09-26 23:09:49 +0900

    change db

--- mongo.js
+++ mongo.js
@@ -7,8 +7,8 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/Todonodek';
-// var url = 'mongodb://heroku_8cn95n6p:3jrhkaspd17dn36bgho8ue7rnk@ds041486.mlab.com:41486/heroku_8cn95n6p';
+// var url = 'mongodb://localhost:27017/Todonodek';
+var url = 'mongodb://heroku_8cn95n6p:3jrhkaspd17dn36bgho8ue7rnk@ds041486.mlab.com:41486/heroku_8cn95n6p';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, mongodb) {
